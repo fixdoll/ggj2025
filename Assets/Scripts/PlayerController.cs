c1f0b4e571eaec2de86b445ed5c75f8dfd2f3f65
fixdoll <fixdoll1@gmail.com>
2025-01-25 13:02:47 +0300

    water dynamics first draft

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,12 +16,16 @@ public class PlayerController : MonoBehaviour
     [Header("Movement Params")]
     public float Speed;
     public float JumpForce;
-    public float PushOffset = 0.02f;
+    public float JumpPushOffset = 0.02f;
+    public float RollPushOffset = 0.02f;
+    public float SwimPushOffset = 0.2f;
     public float TerminalVelocity = 1f;
     public float JumpMultiplier = 2f;
 
     private bool onAir = false;
+    private bool underwater = false;
     private SizeState sizeState;
+    private float dynGrav;
 
     private void Start()
     {
@@ -29,6 +33,8 @@ public class PlayerController : MonoBehaviour
         ThisRigidBody2D.linearDamping = idealDrag / (idealDrag * Time.fixedDeltaTime + 1);
 
         sizeState = SizeState.Small;
+
+        dynGrav = ThisRigidBody2D.gravityScale;
     }
 
     private void Update()
@@ -41,35 +47,44 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float direction = Input.GetAxis("Horizontal");
-    
-        if(sizeState == SizeState.Small)
+        float directionX = Input.GetAxis("Horizontal");
+        float directionY = Input.GetAxis("Vertical");
+
+        if (!underwater)
         {
-            CheckOnAir();
-            if (!onAir)
+            if (sizeState == SizeState.Small)
             {
-                ThisRigidBody2D.linearVelocity = Vector2.zero;
-                var addJump = Input.GetKey(KeyCode.UpArrow);
-                if (Input.GetKey(KeyCode.RightArrow))
+                CheckOnAir();
+                if (!onAir)
                 {
-                    ThisRigidBody2D.AddForceY(JumpForce * (addJump ? JumpMultiplier : 1f), ForceMode2D.Impulse);
-                    ThisRigidBody2D.AddForceX(Speed * 0.5f, ForceMode2D.Impulse);
+                    ThisRigidBody2D.linearVelocity = Vector2.zero;
+                    var addJump = Input.GetKey(KeyCode.UpArrow);
+                    if (Input.GetKey(KeyCode.RightArrow))
+                    {
+                        ThisRigidBody2D.AddForceY(JumpForce * (addJump ? JumpMultiplier : 1f), ForceMode2D.Impulse);
+                        ThisRigidBody2D.AddForceX(Speed * 0.5f, ForceMode2D.Impulse);
+                    }
+                    else if (Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        ThisRigidBody2D.AddForceY(JumpForce * (addJump ? JumpMultiplier : 1), ForceMode2D.Impulse);
+                        ThisRigidBody2D.AddForceX(Speed * -0.5f, ForceMode2D.Impulse);
+                    }
                 }
-                else if (Input.GetKey(KeyCode.LeftArrow))
+                else
                 {
-                    ThisRigidBody2D.AddForceY(JumpForce * (addJump ? JumpMultiplier : 1), ForceMode2D.Impulse);
-                    ThisRigidBody2D.AddForceX(Speed * -0.5f, ForceMode2D.Impulse);
+                    ThisRigidBody2D.AddForceX(Speed * directionX * JumpPushOffset, ForceMode2D.Impulse);
                 }
             }
-            else
+            else if (sizeState == SizeState.Large)
             {
-                ThisRigidBody2D.AddForceX(Speed * direction * PushOffset, ForceMode2D.Impulse);
+                ThisRigidBody2D.AddForceX(Speed * directionX * RollPushOffset, ForceMode2D.Impulse);
             }
         }
-        else if (sizeState == SizeState.Large)
+        else
         {
-            ThisRigidBody2D.AddForceX(Speed * direction * PushOffset, ForceMode2D.Impulse);
+            ThisRigidBody2D.AddForce(new Vector2(Speed * directionX * SwimPushOffset, Speed * directionY * SwimPushOffset));
         }
+        
 
     }
 
@@ -95,11 +110,41 @@ public class PlayerController : MonoBehaviour
         LargeCollider.enabled = isSmall;
         ThisRigidBody2D.freezeRotation = !isSmall;
     }
+
+    void ChangeWater(bool enterWater)
+    {
+        underwater = enterWater;
+        if(enterWater)
+        {
+            ThisRigidBody2D.gravityScale = sizeState == SizeState.Large ? dynGrav * -4f : 0f;
+            ThisRigidBody2D.linearDamping *= 2;
+        }
+        else
+        {
+            ThisRigidBody2D.gravityScale = dynGrav;
+            ThisRigidBody2D.linearDamping /= 2;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.CompareTag("Water"))
+        {
+            ChangeWater(true);
+            Debug.Log("a");
+        }
         if (collision.CompareTag("Target"))
         {
             Debug.LogWarning("[DEBUG] Target hit");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Water"))
+        {
+            ChangeWater(false);
+            Debug.Log("b");
         }
     }
 
