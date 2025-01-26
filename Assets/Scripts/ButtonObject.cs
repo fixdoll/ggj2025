d35b88c1ad077877be21f435929b128e3429d146
menalcaci <menalcaci@gmail.com>
2025-01-26 05:27:02 +0300

    Add new obstacles

--- Assets/Scripts/ButtonObject.cs
+++ Assets/Scripts/ButtonObject.cs
@@ -5,9 +5,21 @@ public class ButtonObject : MonoBehaviour
     public DoorObject DoorToOpen;
     public Animator animator;
 
+
     public void PressButton()
     {
-        //animator.Play("PressAnimation");
+        
+        animator.Play("button_anim");
         DoorToOpen.OpenDoor();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if(collider2D.CompareTag("Player"))
+        {
+            Debug.Log("Player detected");
+            PressButton();
+        }
+        
     }
 }
